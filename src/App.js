382b8d806c2d5c656c602d8279d6121147d21732
Marie Knowlton <mtknowlton67@gmail.com>
2024-03-11 10:21:54 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import WelcomePage from "./components/WelcomePage.js";
+import Header from "./components/Header.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <WelcomePage />
+        <Header/>
       </header>
     </div>
   );
---
+++ src/components/WelcomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function WelcomePage () {
+    return (
+        <section className="welcome-page">
+         <header>
+            <h1>
+             Welcome to the Ultimate Rick and Morty Fan Page!!  
+            </h1>
+         </header>
+        </section>
+    )
+}
