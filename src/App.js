7e0fb1b9a2ac19031f0852487aec9b719ea9f7af
Marie Knowlton <mtknowlton67@gmail.com>
2024-03-13 10:33:33 -0700

    changed project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
-import WelcomePage from "./components/WelcomePage/WelcomePage.js";
-import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+import LandingPage from "./components/LandingPage";
 import './App.css';
 
 export default function App() {
 
   return (
     <main>
-     < WelcomePage />
-    < CharacterCard />
+    <LandingPage />
     </main>
   );
 }
